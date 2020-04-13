6e3cfc299e065b47b2574efd09d845c95c8a3ff0
Hien Pham <hienpham@bravesoft.com.vn>
2020-04-13 11:50:58 +0700

    Initial Commit

---
+++ CustomSuggestionRowSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSuggestionRowSample
+//
+//  Created by Hien Pham on 4/13/20.
+//  Copyright © 2020 Hien Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

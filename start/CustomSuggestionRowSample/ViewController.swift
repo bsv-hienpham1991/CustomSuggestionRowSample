ae3274f25deb81df5d4302afda0a8a37f6bca036
Hien Pham <hienpham@bravesoft.com.vn>
2021-07-05 11:33:56 +0700

    Restructure files and folder

---
+++ start/CustomSuggestionRowSample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CustomSuggestionRowSample
+//
+//  Created by Hien Pham on 4/13/20.
+//  Copyright © 2020 Hien Pham. All rights reserved.
+//
+
+import UIKit
+import Eureka
+import SuggestionRow
+
+class ViewController: FormViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ start/Pods/Target Support Files/Pods-CustomSuggestionRowSampleTests/Pods-CustomSuggestionRowSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomSuggestionRowSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_CustomSuggestionRowSampleTests
+@end
